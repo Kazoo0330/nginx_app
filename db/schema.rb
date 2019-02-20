e895efe84b685842850c84eac5d883b003ec5b44
KazukiSaito <kazoo@kazooPro.local>
2019-02-20 21:50:06 +0900

    :lipstick: blog functionality implemented

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 2019_02_20_124859) do
+
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
